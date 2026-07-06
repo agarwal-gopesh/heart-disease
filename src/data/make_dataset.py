c45020134c86d0e5321d4d2873f0fa68ed24e356
Gopesh <agarwalgopesh6@gmail.com>
2026-07-06 23:31:35 +0530

    Cleaned dataset , added dvc and params.yaml , updated code of make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,76 @@
-import numpy as np 
-from sklearn.datasets import load_iris
+# src/data/make_dataset.py
 
-iris = load_iris()
-X = iris.data
-y = iris.target
+from pathlib import Path
+import yaml
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-print(X.shape, y.shape)
+
+def load_params():
+    root = Path(__file__).resolve().parents[2]
+
+    with open(root / "params.yaml") as f:
+        params = yaml.safe_load(f)
+
+    return params["make_dataset"], root
+
+
+def load_data(data_path):
+    return pd.read_csv(data_path)
+
+
+def split_data(df, test_size, random_state):
+    train, test = train_test_split(
+        df,
+        test_size=test_size,
+        random_state=random_state
+    )
+
+    return train, test
+
+
+def save_data(train, test, output_path):
+
+    output_path.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    train.to_csv(
+        output_path / "train.csv",
+        index=False
+    )
+
+    test.to_csv(
+        output_path / "test.csv",
+        index=False
+    )
+
+
+def main():
+
+    params, root = load_params()
+
+    input_file = sys.argv[1]
+
+    data_path = root / input_file
+    output_path = root / "data" / "processed"
+
+    data = load_data(data_path)
+
+    train, test = split_data(
+        data,
+        params["test_split"],
+        params["seed"]
+    )
+
+    save_data(
+        train,
+        test,
+        output_path
+    )
+
+
+if __name__ == "__main__":
+    main()
